1fff7a142842346e48ca862bad5e460a0bbe0ef3
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-10-16 22:38:12 -0300

    correção

--- src/componentes/tabelaStars.js
+++ src/componentes/tabelaStars.js
@@ -2,37 +2,70 @@ import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 import './componentes.css'
+import { Button } from 'reactstrap';
+
+var page = 0
+var url = {
+    stars1: '> 9279',
+    stars2: ' 8000..9279',
+    forks1: '>2050',
+    forks2: '1500..2050',
+}
 
 export default class Table extends Component {
     constructor() {
         super()
-        this.state = {arrayProjeto:[],lecense:0,linguagem:0}
+        this.state = {arrayProjeto:[],lecense:0,linguagem:0, continua: true}
+        this.download = this.download.bind(this)
     }
     componentDidMount() {  
-        for(var i=10; i>=1; i-- ){
-            //fetch(`https://api.github.com/search/repositories?q=stars%3A%3E9000&per_page=100&page= ${i}` )
-            fetch(`https://api.github.com/search/repositories?q=forks%3A>1000&per_page=100&page= ${i}` )
-            .then(response => response.json())
-            .then(projetos => {
-                projetos.items.map(function (proj) {  //verifica se possui licença
-                    proj.license = proj.license ? proj.license.name : 'null'
-                    return true
-                }
-                )                
-                for(let i=0; i<100; i++ ){   
-                    this.setState({ arrayProjeto: this.state.arrayProjeto.concat(projetos.items[i]) })
-                }
-            })
-            
+        // this.fetchQuotes()
+        this.timer = setInterval(() => this.fetchQuotes(), 5000)
+    }
+    fetchQuotes(){
+        page++
+        if(this.state.continua){
+            //fetch(`https://api.github.com/search/repositories?q=stars%3A${page > 10 ? url.stars2 : url.stars1}&per_page=100&page= ${page > 10?page - 10: page }` )
+            fetch(`https://api.github.com/search/repositories?q=forks%3A${page >= 11 ? url.forks2 : url.forks1}&per_page=2000&page= ${page > 10 ? page - 10 : page }`)
+                .then(response => response.json())
+                .then(projetos => {
+                    console.log(page)
+                    console.log(projetos)
+                    projetos.items.map(function (proj) {  //verifica se possui licença
+                        proj.license = proj.license ? proj.license.name : 'null'
+                        return true
+                    })
+                    for (let i = 0; i < projetos.items.length; i++) {
+                        if (projetos.items[i].language != null && this.state.arrayProjeto.length <= 999) {
+                            this.setState({ arrayProjeto: this.state.arrayProjeto.concat(projetos.items[i]) })
+                        }
+
+                        if (this.state.arrayProjeto.length === 999) {
+                            console.log('fumego')
+                            this.setState({continua : false})
+                        }
+                    }
+                }).catch(error =>{
+                    page--
+                    console.log('Erro ->' +  error)
+                })
         }
-        
+    }
+    componentWillUnmount(){
+        this.timer = null
     }
 
+    download(){
+        // var data = 'data:text/json;charset=utf-8,' + JSON.stringify(this.state.arrayProjeto)
+        // window.open(encodeURI(data))
 
+        var fileDownload = require('js-file-download');
+        fileDownload(JSON.stringify(this.state.arrayProjeto), 'Forks.json');
+    }
     render() {
-              
         return (
             <div id='tabela' >
+                <Button color="secondary" onClick = {this.download} >Luiz</Button>
                 <BootstrapTable data={ this.state.arrayProjeto} exportCSV={true} pagination>
                     <TableHeaderColumn dataField='id'               dataSort={ true } width='0%' isKey > Product ID          </TableHeaderColumn>
                     <TableHeaderColumn dataField='name'             dataSort={ true }> name                </TableHeaderColumn>
@@ -41,12 +74,10 @@ export default class Table extends Component {
                    
                     <TableHeaderColumn dataField='stargazers_count' dataSort={true}> stargazers_count      </TableHeaderColumn>
                     <TableHeaderColumn dataField='forks_count'      dataSort={ true }> forks_count         </TableHeaderColumn>
-                    <TableHeaderColumn dataField='open_issues_count'dataSort={ true }> open_issues_count   </TableHeaderColumn>
                     <TableHeaderColumn dataField='license'          dataSort={ true }> license             </TableHeaderColumn>
                     <TableHeaderColumn dataField='language'         dataSort={ true }> linguagem           </TableHeaderColumn>
                     <TableHeaderColumn dataField='open_issues_count'dataSort={ true }> open_issues_count   </TableHeaderColumn>
                     <TableHeaderColumn dataField='fork'             dataSort={ true }> fork                </TableHeaderColumn>
-
                 </BootstrapTable>
             </div>
         )
